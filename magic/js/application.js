8adb229e87073fc5b3c1cfce9be880cec87f33dc
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-06 07:35:31 +0200

    Control card

--- magic/js/application.js
+++ magic/js/application.js
@@ -1,6 +1,10 @@
-/*global Reveal*/
-(function(Reveal){
+/*global window, document, Reveal, deck*/
+(function(Reveal, deck){
+    'use strict';
     Reveal.addEventListener('card-trick', function(){
-        console.log('card trick');
+        var container = document.getElementById('deck');
+        var view = new deck.View(container);
+        window.view = view;
+        view.show('3H');
     });
-})(Reveal);
+})(Reveal, deck);
